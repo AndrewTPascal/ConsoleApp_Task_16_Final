d865538f0087e78e11bb65cb22ebace3e79fb06f
AndrewTPascal <andrvotkinski@yandex.ru>
2023-04-04 23:39:00 +0400

    Добавьте файлы проекта.

---
+++ PracticesTest/Class1.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace Practices.Tests
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public void Additional_MustReturnCorrectValue() 
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Additional(2, 2) == 4);
+        }
+
+        [Test]
+        public void Subtraction_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Subtraction(1, 5) == -4);
+        }
+
+        [Test]
+        public void Miltiplication_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Miltiplication(5, 0) == 0);
+        }
+
+        [Test]
+        public void Division_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Division(5, 1) == 5);
+        }
+    }
+}
